96a99068402fbe16d58bdf39ec770deb88f731e5
Michael Klotzner <michael.klotzner@student.tugraz.at>
2015-11-25 19:16:33 +0100

    Added main content and copyright to index.php page.

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gonimo - First web-based baby monitor!</title>
+</head>
+<body>
+	<h1>Gonimo.com</h1>
+	<p>Good Night Monitor</p>
+	<p>The first web-based baby monitor!</p>
+
+	<h2>DEVELOPMENT BLOG</h2>
+	<p>You can find news about current development on our <a href="http://blog.gonimo.com/">development blog</a>.</p>
+
+	<h2>DEVELOPMENT STATUS</h2>
+	<p>For detailed information about the current development status and what we are currently working on you can visit our <a href="https://tree.taiga.io/project/eskimo-gonimo/backlog">taiga</a> instance.</p>
+	<p>More information about our sprints can be found <a href="https://tree.taiga.io/project/eskimo-gonimo/wiki/sprints">here</a>.</p>
+
+	<h2>USE IT!</h2>
+	<p>After every development sprint, you will find the current version up and running <a href="http://baby.gonimo.com/">here</a>.</p>
+	<p>Well this link currently does not work, as we have not finished any sprint yet, in the meantime you can try our old prototype <a href="http://mybabymonitor.org/">here</a>.</p>
+
+	<h2>SOURCE CODE</h2>
+	<p>gonimo is free and open source software! You can find the code on <a href="https://github.com/eskimor/mybabymonitor.org">github</a>.</p>
+
+	<h2>GOAL OF GONIMO.COM</h2>
+	<p>Create a superior free baby monitor solution that works in the browser, without any installation or plugins.</p>
+	<p>It will ...</p>
+	<ul>
+		<li>be free and open source</li>
+		<li>be reliable:</li>
+			<ul>
+				<li>automatic reconnections</li>
+				<li>battery warning (baby &amp; parent station will be monitored)</li>
+				<li>alert on connection loss</li>
+				<li>permanent connection monitoring</li>
+			</ul>
+		<li>be as secure as reasonable possible:</li>
+			<ul>
+				<li>All data that is needed is stored on the client</li>
+				<li>Intelligent two server solution: An attacker has to crack two servers in order to do any serious harm.</li>
+				<li>An intrusion will be noticed immediately</li>
+			</ul>
+		<li>be easy to use</li>
+		<li>be beautiful &amp; cute to look at</li>
+		<li>work on all important browsers</li>
+		<li>offer"Push to talk" to your baby</li>
+		<li>allow multiple parent devices to connect simultaneously to the same baby device</li>
+		<li>offer Sound level visualization: "See" your baby scream</li>
+		<li>offer Android Wrapper application. Find it on Google Play, maybe additional features.</li>
+		<li>be a great replacement for traditional devices &amp; native apps</li>
+	</ul>
+	<footer>Copyright &copy; <?php if(date("Y") != "2015") echo "2015 - " . date("Y"); else echo "2015"; ?> The Gonimo Group</footer>
+</body>
+</html>
